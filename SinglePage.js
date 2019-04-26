16ebf75b769594e5ac42204939b275a903a2dab2
KV Nguyen <kieuvyn12@gmail.com>
2019-04-26 13:02:25 -0400

    added single page to react navigation

--- SinglePage.js
+++ SinglePage.js
@@ -2,14 +2,10 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class HomeScreen extends Component {
-  onPress = () => {
-    this.props.navigation.push('MainPage');
-  };
-
   render() {
     return (
       <View>
-        <Text onPress={this.onPress}>Welcome to Remoter!</Text>
+        <Text>What you clicked on will show up here!</Text>
       </View>
     );
   }
